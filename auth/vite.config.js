dc2892b053571fe4e221da2e605fbf2574e271ef
Gwendolyn <me@gwendolyn.dev>
2024-08-05 21:59:23 +0200

    thingses

--- auth/vite.config.js
+++ auth/vite.config.js
@@ -4,4 +4,15 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    manifest: true,
+    assetsDir: 'static',
+    rollupOptions: {
+      input: 'auth.html'
+    }
+  },
+  server: {
+    open: 'auth.html'
+  }
+
 })
