090ef5ff8db106ba27762fc28514b2a663200526
Gwendolyn <me@gwendolyn.dev>
2024-08-18 16:20:41 +0200

    slightly prettier gradients using oklch interpolation

--- components/src/LoadingBar.vue
+++ components/src/LoadingBar.vue
@@ -9,7 +9,7 @@ const props = defineProps({
 </script>
 
 <template>
-  <div class="w-full h-1" :class="{'is-loading': isLoading}">
+  <div class="w-full h-1" :class="{'is-loading': true}">
   </div>
 </template>
 
@@ -21,6 +21,7 @@ const props = defineProps({
 .is-loading::before {
   content: '';
   @apply absolute top-0 left-0 h-full w-full bg-gradient-to-r from-fuchsia-300 via-purple-800 to-fuchsia-300;
+  background-image: linear-gradient(in oklch to right, var(--tw-gradient-stops));
   background-size: 200% 100%;
   animation: loading-bar 1.5s linear infinite;
 }
